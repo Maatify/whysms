1933d0b3c528558a36ef9a9cd3bc296dc5248c38
Mohamed Abdulalim <megyptm@gmail.com>
2023-07-30 17:42:13 +0300

    Initial commit

---
+++ src/Request.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * @copyright   ©2023 Maatify.dev
+ * @Liberary    WhySms
+ * @Project     WhySms
+ * @author      Mohamed Abdulalim (megyptm) <mohamed@maatify.dev>
+ * @since       2023-07-30 11:39 AM
+ * @see         https://www.maatify.dev Maatify.com
+ * @link        https://github.com/Maatify/whysms  view project on GitHub
+ * @link        https://github.com/Maatify/Logger (maatify/logger)
+ * @copyright   ©2023 Maatify.dev
+ * @note        This Project using for WhySMS Egypt SMS Provider API.
+ * @note        This Project extends other libraries maatify/logger.
+ *
+ * @note        This program is distributed in the hope that it will be useful - WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+namespace Maatify\WhySms;
+
+use Maatify\Logger\Logger;
+
+abstract class Request
+{
+    protected string $url;
+
+    protected string $api_token;
+
+    protected string $sender_id;
+
+    protected function Curl(array $params = []){
+        if(!empty($this->url)) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $this->url);
+            if(!empty($params)){
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+            }else{
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+            }
+            /*
+            // no need returns from curl
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+            curl_setopt($ch, CURLOPT_TIMEOUT_MS, 100);
+            */
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+            curl_setopt($ch, CURLOPT_FAILONERROR, true); // Required for HTTP error codes to be reported via our call to curl_error($ch)
+            //        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Authorization: Bearer ' . $this->api_token,
+                'Cache-Control: no-cache',
+                'Content-Type: application/json',
+            ));
+                $result = curl_exec($ch);
+                $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                $curl_errno = curl_errno($ch);
+                $curl_error = curl_error($ch);
+                curl_close($ch);
+
+            if ($curl_errno > 0) {
+                $response['success'] = false;
+                $response['error'] = "(err-" . __METHOD__ . ") cURL Error ($curl_errno): $curl_error";
+            } else {
+                if ($resultArray = json_decode($result, true)) {
+                    Logger::RecordLog([
+                        $resultArray,
+                        $this->url,
+                        __METHOD__,], 'Debug_' . __FUNCTION__);
+                    $response = $resultArray;
+//                    $response['success'] = true;
+                } else {
+                    $response['success'] = false;
+                    $response['error'] = ($httpCode != 200) ? "Error header response " . $httpCode : "There is no response from server (err-" . __METHOD__ . ")";
+                    $response['result'] = $result;
+                }
+            }
+
+            if (empty($response['success'])) {
+                Logger::RecordLog([
+                    $response,
+                    $this->url,
+                   __METHOD__], 'Debug_' . __FUNCTION__);
+            }
+
+            return $response;
+        }
+        return ['success' => false];
+    }
+
+}
---
+++ src/WhySms.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @copyright   ©2023 Maatify.dev
+ * @Liberary    WhySms
+ * @Project     WhySms
+ * @author      Mohamed Abdulalim (megyptm) <mohamed@maatify.dev>
+ * @since       2023-07-30 12:02 PM
+ * @see         https://www.maatify.dev Maatify.com
+ * @link        https://github.com/Maatify/whysms  view project on GitHub
+ * @link        https://github.com/Maatify/Logger (maatify/logger)
+ * @copyright   ©2023 Maatify.dev
+ * @note        This Project using for WhySMS Egypt SMS Provider API.
+ * @note        This Project extends other libraries maatify/logger.
+ *
+ * @note        This program is distributed in the hope that it will be useful - WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+namespace Maatify\WhySms;
+
+class WhySms extends Request
+{
+    private static self $instance;
+
+    public static function obj(string $api_key, string $sender_name): self
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new self($api_key, $sender_name);
+        }
+
+        return self::$instance;
+    }
+
+    public function __construct(string $api_key, string $sender_name)
+    {
+        $this->sender_id = $sender_name;
+        $this->api_token = $api_key;
+    }
+
+    private string $url_v3 = 'https://bulk.whysms.com/api/v3/';
+
+    public function CheckBalance(): array
+    {
+        $this->url = $this->url_v3 . 'balance';
+        $response = $this->Curl();
+        if(!empty($response['success']) && !empty($response['data'])){
+            return [
+                'success'        => true,
+                'remaining_unit' => $response['data']['remaining_unit'] ?? '',
+                'expired_on'     => $response['data']['expired_on'] ?? '',
+            ];
+        }
+        return [
+            'success' => false
+        ];
+    }
+
+    public function SendSms(string $phone, string $message): array
+    {
+        $this->url = $this->url_v3 . 'sms/send';
+
+        $response = $this->Curl(
+            [
+                'recipient' => $phone,
+                'sender_id' => $this->sender_id,
+                'type'      => 'plain',
+                'message'   => $message,
+            ]
+        );
+        if(!empty($response['success']) && !empty($response['data'])){
+            return [
+                'success' => true,
+                'details' => $response['message'] ?? '',
+                'uid'     => $response['data']['uid'] ?? '',
+                'to'      => $response['data']['to'] ?? '',
+                'from'    => $response['data']['from'] ?? '',
+                'message' => $response['data']['message'] ?? '',
+                'status'  => $response['data']['status'] ?? '',
+                'cost'    => $response['data']['cost'] ?? '',
+            ];
+        }
+        return [
+            'success' => false
+        ];
+    }
+}
